5cd7de51a517e6845e773330a759b5cfb11670c7
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-01-22 17:26:27 +0530

    Fix bug on numbering last questions

--- exit.php
+++ exit.php
@@ -8,7 +8,10 @@ require("connect.php");
 
 
 mysqli_query($connection, "UPDATE student_auth SET track=1 WHERE username='$username'") or die ("something went wrong with track");
+$query = mysqli_query($connection,"SELECT * FROM '$username'");
+$assoc = mysqli_fetch_assoc($query);
 
+print_r($assoc);
 
 if(session_destroy()) // Destroying All Sessions
 {
--- quiz17res.php
+++ quiz17res.php
@@ -1,14 +1,13 @@
 <?php
 
 
- 		$answer1 = $_POST["question-121-answers"];
- 		$answer2 = $_POST['question-122-answers'];
-        $answer3 = $_POST['question-123-answers'];
-        $answer4 = $_POST['question-124-answers'];
-        $answer5 = $_POST['question-125-answers'];
-        $answer6 = $_POST['question-126-answers'];
-        $answer7 = $_POST['question-127-answers'];
-        $answer8 = $_POST['question-128-answers'];
+ 		$answer1 = $_POST["question-128-answers"];
+ 		$answer2 = $_POST['question-129-answers'];
+        $answer3 = $_POST['question-130-answers'];
+        $answer4 = $_POST['question-131-answers'];
+        $answer5 = $_POST['question-132-answers'];
+        $answer6 = $_POST['question-133-answers'];
+        $answer7 = $_POST['question-134-answers'];
 
    session_start();
 
@@ -23,14 +22,14 @@
 
 
 
-mysqli_query($connection, "UPDATE $username SET response='$answer1' WHERE question=121");
-mysqli_query($connection, "UPDATE $username SET response='$answer2' WHERE question=122");
-mysqli_query($connection, "UPDATE $username SET response='$answer3' WHERE question=123");
-mysqli_query($connection, "UPDATE $username SET response='$answer4' WHERE question=124");
-mysqli_query($connection, "UPDATE $username SET response='$answer5' WHERE question=125");
-mysqli_query($connection, "UPDATE $username SET response='$answer6' WHERE question=126");
-mysqli_query($connection, "UPDATE $username SET response='$answer7' WHERE question=127");
-mysqli_query($connection, "UPDATE $username SET response='$answer8' WHERE question=128");
+mysqli_query($connection, "UPDATE $username SET response='$answer1' WHERE question=128");
+mysqli_query($connection, "UPDATE $username SET response='$answer2' WHERE question=129");
+mysqli_query($connection, "UPDATE $username SET response='$answer3' WHERE question=130");
+mysqli_query($connection, "UPDATE $username SET response='$answer4' WHERE question=131");
+mysqli_query($connection, "UPDATE $username SET response='$answer5' WHERE question=132");
+mysqli_query($connection, "UPDATE $username SET response='$answer6' WHERE question=133");
+mysqli_query($connection, "UPDATE $username SET response='$answer7' WHERE question=134");
+
 
     	$totalScore = 0;
     
